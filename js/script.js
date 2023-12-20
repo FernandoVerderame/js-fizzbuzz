f9216bfa2b8073bf29f4377a641948b23c8053f7
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-20 18:11:36 +0100

    added elements in HTML by template literal

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
+// ! TEMPLATE LITERAL
+const element = document.getElementById('target');
+
 // Dichiariamo due variabili
 x = 3;
 y = 5;
+
+let listSquare = '<div>';
 
 // Creo un ciclo di numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
@@ -8,20 +13,31 @@ for (let i = 1; i <= 100; i++) {
     // Numero NON divisibile ne per 3 ne per 5
     if (!(i % x == 0) && !(i % y == 0)) {
         console.log(i);
+        listSquare += `<div>${i}</div>`;
 
     } 
         // Numero divisibile sia per 3 che 5
         else if ((i % x == 0) && (i % y == 0)) {
         console.log('FizzBuzz');
+        listSquare += '<div>FizzBuzz</div>';
 
     } 
         // Numero dibisibile per 3
         else if (i % x == 0) {
         console.log('Fizz');
+        listSquare += '<div>Fizz</div>';
 
     } 
         // Numero divisibile per 5
         else if (i % y == 0) {
         console.log('Buzz');
+        listSquare += '<div>Buzz</div>';
+
     }
 }
+
+listSquare += '</div>';
+
+console.log(listSquare);
+
+element.innerHTML = listSquare;
