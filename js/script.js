bce56342381029132e1d8f55779225ab196d07bc
FernandoVerderame <fernandoverderame96@hotmail.com>
2023-12-20 17:45:47 +0100

    number not divisible by 3 and 5

--- js/script.js
+++ js/script.js
@@ -5,4 +5,9 @@ y = 5;
 // Creo un ciclo di numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
 
+    // Numero NON divisibile ne per 3 ne per 5
+    if (!(i % x == 0) && !(i % y == 0)) {
+        console.log(i);
+
+    } 
 }
